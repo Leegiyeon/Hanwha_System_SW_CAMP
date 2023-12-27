8fa46d96eaa407cbcc80f6074ae9df80a57f0614
Leegiyeon <rldus3512@naver.com>
2023-12-27 12:43:45 +0900

    파일 전반 수정

--- Class/10_dml.sql
+++ Class/10_dml.sql
@@ -1,33 +1,6 @@
 DESC tbl_menu;
 
-INSERT 
-  INTO tbl_menu
-(
-  menu_code
-, menu_name
-, menu_price
-, category_code
-, orderable_status
-)
-VALUES 
-(
-  NULL
-, '바나나해장국'
-, 8500 
-, 4
-, 'Y'
-);
-
-SELECT * FROM tbl_menu ORDER BY 1 DESC;
--- -------------------------------------
-UPDATE tbl_menu
-	SET menu_name = '딸기 해장국'
- WHERE menu_code = 22;
- 
-SELECT * FROM tbl_menu ORDER BY 1 DESC;
-
--- -------------------------------------
-REPLACE
+INSERT
   INTO tbl_menu
 (
   menu_code
@@ -38,6 +11,43 @@ REPLACE
 )
 VALUES
 (
+  NULL
+, '바나나해장국'
+, 8500
+, 4
+, 'Y'
+);
+
+SELECT * FROM tbl_menu ORDER BY 1 DESC;
+
+-- ----------------------------------------
+-- UPDATE
+UPDATE tbl_menu
+   SET menu_name = '딸기해장국'
+ WHERE menu_code = 22;
+ 
+-- ----------------------------------------
+REPLACE
+  INTO tbl_menu
+(
+  menu_code, menu_name, menu_price
+, category_code, orderable_status
+)
+VALUES
+(
   17, '아샷추매운탕', 9000
 , 4, 'N'
 );
+
+SELECT * FROM tbl_menu WHERE menu_code = 17;
+
+
+ 
+
+  
+  
+  
+  
+  
+  
+
--- Class/11_transaction.sql
+++ Class/11_transaction.sql
@@ -5,7 +5,7 @@ START TRANSACTION;
 
 INSERT 
   INTO tbl_menu
-VALUES 
+VALUES
 (
   NULL, '바나나해장국', 8500
 , 4, 'Y'
@@ -13,15 +13,15 @@ VALUES
 
 UPDATE tbl_menu
    SET menu_name = '수정된 메뉴1'
- WHERE menu_code = '5';
+ WHERE menu_code = 5;
  
-
 UPDATE tbl_menu
    SET menu_name = '수정된 메뉴2'
- WHERE menu_code = '6';
+ WHERE menu_code = 6;
  
-SELECT * FROM tbl_menu; -- 조회된 데이터는 실제 DB에 적용된 값이 아니다.
+SELECT * FROM tbl_menu;
 
 ROLLBACK;
+ 
 
-SELECT * FROM tbl_menu; -- rollback 또는 commit 이후 select 한 것이 실제 DB에 적용된 값이다.
+  
