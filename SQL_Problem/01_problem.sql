c8bf03edae3f5bab4540a64bcece688e63a2fed8
Leegiyeon <74126467+Leegiyeon@users.noreply.github.com>
2023-12-28 07:39:54 +0900

    SQL_problem01
    한문제 추가

--- SQL_Problem/01_problem.sql
+++ SQL_Problem/01_problem.sql
@@ -13,3 +13,13 @@ WHERE USER_ID IN (
     HAVING COUNT(*) >=3
 )
 ORDER BY USER_ID DESC;
+
+
+-- 상반기 동안 각 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량을 총주문량이 작은 순서대로 조회하는 SQL 문
+SELECT
+       b.INGREDIENT_TYPE
+     , sum(a.TOTAL_ORDER) as TOTAL_ORDER
+  from First_half a
+  join icecream_info b on (a.flavor = b.flavor)
+ group by b.ingredient_type
+ order by TOTAL_ORDER;
