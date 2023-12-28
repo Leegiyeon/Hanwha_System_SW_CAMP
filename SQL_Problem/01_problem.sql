9ea8028a5cd4a9aaac1ce2fb42163bf5db78ca75
Leegiyeon <74126467+Leegiyeon@users.noreply.github.com>
2023-12-29 08:27:10 +0900

    SQL_problem02

--- SQL_Problem/01_problem.sql
+++ SQL_Problem/01_problem.sql
@@ -16,7 +16,7 @@ SELECT USER_ID
 SELECT
        b.INGREDIENT_TYPE
      , sum(a.TOTAL_ORDER) AS TOTAL_ORDER
-  FROM First_half a
+  FROM first_half a
   JOIN icecream_info b ON (a.flavor = b.flavor)
  GROUP BY b.ingredient_type
  ORDER BY TOTAL_ORDER;
