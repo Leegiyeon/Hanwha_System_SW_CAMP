12af8eee9a6e32e95c62e875634dbf3bb8631c63
Leegiyeon <74126467+Leegiyeon@users.noreply.github.com>
2023-12-29 08:24:11 +0900

    SQL_problem02

--- SQL_Problem/01_problem.sql
+++ SQL_Problem/01_problem.sql
@@ -12,7 +12,8 @@ SELECT USER_ID
  ORDER BY USER_ID DESC;
 
 
--- 상반기 동안 각 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량을 총주문량이 작은 순서대로 조회하는 SQL 문
+-- 상반기 동안 각 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량을
+-- 총주문량이 작은 순서대로 조회하는 SQL 문
 SELECT
        b.INGREDIENT_TYPE
      , sum(a.TOTAL_ORDER) AS TOTAL_ORDER
