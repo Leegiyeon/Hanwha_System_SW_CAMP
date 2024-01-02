c9ebba7c979c6fdf99c142eec3314b23dad90912
Leegiyeon <74126467+Leegiyeon@users.noreply.github.com>
2024-01-03 03:46:20 +0900

    Update 02_problem.sql

--- SQL_Problem/02_problem.sql
+++ SQL_Problem/02_problem.sql
@@ -8,4 +8,20 @@ SELECT
 HAVING AVERAGE_DURATION >= 7
  ORDER BY AVERAGE_DURATION DESC, car_id DESC;
 
---
+-- 2021년에 가입한 전체 회원들 중
+-- 상품을 구매한 회원수와 상품을 구매한 회원의 비율
+-- (=2021년에 가입한 회원 중 상품을 구매한 회원수 / 2021년에 가입한 전체 회원 수)을 년, 월 별로 출력
+SELECT YEAR(a.SALES_DATE)  AS YEAR
+     , MONTH(a.SALES_DATE) AS MONTH
+     , COUNT(DISTINCT a.USER_ID) AS PUCHASED_USERS
+     , ROUND(COUNT(DISTINCT a.USER_ID)/(SELECT COUNT(1)
+                                            FROM USER_INFO
+                                           WHERE JOINED
+                                            LIKE '2021%'), 1) AS PUCHASED_RATIO
+  FROM ONLINE_SALE a
+     , USER_INFO   b
+ WHERE 1 = 1
+   AND a.USER_ID = b.USER_ID
+   AND b.JOINED  LIKE '2021%'
+ GROUP BY YEAR, MONTH
+ ORDER BY 1, 2;
