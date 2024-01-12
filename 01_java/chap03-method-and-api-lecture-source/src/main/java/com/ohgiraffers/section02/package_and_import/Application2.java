6892214fc29eddb2fb51ab68264107ffecdb09b0
Leegiyeon <rldus3512@naver.com>
2024-01-12 11:04:36 +0900

    Java
    package, import 학습내용 추가

--- 01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application2.java
+++ 01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application2.java
@@ -5,6 +5,8 @@ public class Application2 {
     public static void main(String[] args) {
 
         /* 수업목표. import에 대해 이해할 수 있다. */
+
+        /* 목차. 1. non-static method */
         Calculator cal = new Calculator(); // import를 활용해 줄여쓴 Calculator
         int first = 100;
         int second = 200;
@@ -12,5 +14,8 @@ public class Application2 {
 
         System.out.println("두 수의 합: " + cal.plusTwoNums(first, second));
         System.out.println("두 수의 차: " + sub);
+
+        /* 목차. 2. static method */
+        System.out.println("두 수 중 큰 값: " + Calculator.maxTwoNums(first, second));
     }
 }
