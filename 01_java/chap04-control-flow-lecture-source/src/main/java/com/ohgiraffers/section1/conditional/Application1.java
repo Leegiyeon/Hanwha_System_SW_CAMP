0e483e912a444ed756d30a31e565409f7903ef7c
Leegiyeon <rldus3512@naver.com>
2024-01-12 15:37:13 +0900

    Java(if)
    조건문 학습내용 Upload

--- 01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section1/conditional/Application1.java
+++ 01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section1/conditional/Application1.java
@@ -3,23 +3,23 @@ package com.ohgiraffers.section1.conditional;
 public class Application1 {
     /* 설명. 이제부터 실행용 클래스를 구분해서 사용한다.(main 메소드는 여기에만 생성) */
     public static void main(String[] args) {
-//        A_if aClass = new A_if();
-//        // 필기. 단독 if문 흐름 확인
-//        aClass.testSimpleIfStatement();
-//        // 필기. 중첩 if문 흐름 확인
-//        aClass.testNestedIfStatement();
-//
-//
-//        B_ifElse bClass = new B_ifElse();
-//        // 필기. 단독 if-else문 흐름 확인
-//        bClass.testSimpleIfElseStatement();
-//        // 필기. 중첩 if-else문 흐름 확인
-//        bClass.testNestedIfElseStatement();
-//
-//
+        A_if aClass = new A_if();
+        // 필기. 단독 if문 흐름 확인
+        aClass.testSimpleIfStatement();
+        // 필기. 중첩 if문 흐름 확인
+        aClass.testNestedIfStatement();
+
+
+        B_ifElse bClass = new B_ifElse();
+        // 필기. 단독 if-else문 흐름 확인
+        bClass.testSimpleIfElseStatement();
+        // 필기. 중첩 if-else문 흐름 확인
+        bClass.testNestedIfElseStatement();
+
+
         C_ifElseIf cClass = new C_ifElseIf();
-//        // 필기. 단독 if-else if문 흐름 확인
-//        cClass.testSimpleIfElseIfStatement();
+        // 필기. 단독 if-else if문 흐름 확인
+        cClass.testSimpleIfElseIfStatement();
         // 필기. 중첩 if-else if문 흐름 확인
         cClass.testNestedIfElseIfStatement();
     }
