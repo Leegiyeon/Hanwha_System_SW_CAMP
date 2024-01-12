0c8714b302c3fd6e3ac47189f0304d3825406441
Leegiyeon <rldus3512@naver.com>
2024-01-12 10:49:43 +0900

    Java
    package, import 학습내용 추가

---
+++ 01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application2.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section02.package_and_import;
+
+import com.ohgiraffers.section01.method.Calculator; // 설명. import는 경로를 생략하기 위해 작성 (줄여 쓰기 위함)
+public class Application2 {
+    public static void main(String[] args) {
+
+        /* 수업목표. import에 대해 이해할 수 있다. */
+        Calculator cal = new Calculator(); // import를 활용해 줄여쓴 Calculator
+        int first = 100;
+        int second = 200;
+        int sub = cal.subTwoNums(first, second);
+
+        System.out.println("두 수의 합: " + cal.plusTwoNums(first, second));
+        System.out.println("두 수의 차: " + sub);
+    }
+}
