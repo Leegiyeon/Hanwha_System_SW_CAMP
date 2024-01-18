b29f05dcce4894fff9ceaee5503c0911ce86fd9e
Leegiyeon <rldus3512@naver.com>
2024-01-18 10:41:35 +0900

    Java
    Atm 프로젝트 실습 구현 내용 Upload

--- 01_java/atm/src/main/java/atm/AtmUI.java
+++ 01_java/atm/src/main/java/atm/AtmUI.java
@@ -8,47 +8,47 @@ public class AtmUI {
 
         Scanner sc = new Scanner(System.in);
         boolean isOn = true;
-        boolean login = false;
+        boolean pass = false;
         int idx = 0; //member index
         Member member = new Member();
+        member.memberManager(); // 기존 가입 member load
 
 
         while (isOn) {
+
             System.out.println("====    ATM    ====");
             System.out.println("==== 1.  입금   ====");
             System.out.println("==== 2.  출금   ====");
             System.out.println("==== 3. 잔액조회 ====");
-            System.out.println("==== 4. 회원가입 ====");
+//            System.out.println("==== 4. 회원가입 ====");
             System.out.println("==== 9.  종료   ====");
 
-            System.out.println("PLEASE ENTER THE NUMBER: ");
+            System.out.print("PLEASE ENTER THE NUMBER: ");
 
             int input = sc.nextInt();
 
 
-
             // 각 기능 별 접근 시, 로그인 추후 개발 예정
             if (input == 1) {
-                member.login();
-                Deposit deposit = new Deposit();
-                deposit.howMuch();
-
+                if (member.login(pass)) {
+                    member.deposit();
+                }
             } else if (input == 2) {
-                member.login();
-                Withdraw withdraw = new Withdraw();
-                withdraw.howMuch();
+                if (member.login(pass)) {
+                    member.withdraw();
+                }
             } else if (input == 3) {
-                member.login();
-                Balance balance = new Balance();
-                balance.printBalance();
-            } else if (input == 4) {
-                member.memberManager();
-                member.signUp(idx);
-            } else if (input == 9) {
-                System.out.println("ATM이 종료됩니다.");
-                isOn = false;
+                if (member.login(pass)) {
+                    member.getBalance();
+//            } else if (input == 4) {
+//                member.memberManager();
+//                member.signUp(idx);
+                } else if (input == 9) {
+                    System.out.println("ATM이 종료됩니다.");
+                    isOn = false;
+                }
+                idx++;
             }
-            idx++;
         }
     }
 }
--- 01_java/atm/src/main/java/atm/Member.java
+++ 01_java/atm/src/main/java/atm/Member.java
@@ -1,10 +1,21 @@
 package atm;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Member {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private int accountNumber;
     private String name;
     private String pwd;
@@ -16,8 +27,8 @@ public class Member {
     Member[] members = new Member[6];
 
 
-
-    public Member(int accountNumber, String name, String pwd, double balance) {
+    public Member(int id, int accountNumber, String name, String pwd, double balance) {
+        this.id = id;
         this.accountNumber = accountNumber;
         this.name = name;
         this.pwd = pwd;
@@ -49,6 +60,8 @@ public class Member {
     }
 
     public double getBalance() {
+        System.out.println("고객님 계좌의 잔액은" + members[id].balance + "원 입니다.");
+        System.out.println("서비스를 이용해주셔서 감사합니다.");
         return balance;
     }
 
@@ -73,31 +86,86 @@ public class Member {
         name = sc.nextLine();
         System.out.print("사용하실 비밀번호를 입력해주세요: ");
         pwd = sc.nextLine();
-        members[id] = new Member(accountNumber, name, pwd, 0);
+        members[id] = new Member(id, accountNumber, name, pwd, 0);
         System.out.println("가입된 회원정보는 " + Arrays.toString(members) + " 입니다.");
         System.out.println("가입된 회원정보는 " + members[id] + " 입니다.");
     }
 
-    public void login() {
+    public boolean login(boolean login) {
         Scanner sc = new Scanner(System.in);
         System.out.print("계좌번호를 입력해주세요: ");
         accountNumber = sc.nextInt();
 
+//        System.out.println("member account: " + members[3].accountNumber);
 
-//        if(members.equals(members[].accountNumber){
-//            System.out.print("비밀번호를 입력해주세요: ");
-//            pwd = sc.nextLine();
-//        }
+        id = 0;
+        for (int i = 0; i < members.length; i++) {
+            if (accountNumber == members[i].accountNumber) {
+                id = i;
+                System.out.print("비밀번호를 입력해주세요: ");
+                pwd = sc.next();
+                System.out.println("member pwd: " + members[id].pwd);
+                System.out.println("pwd: " + pwd);
+                if (Objects.equals(pwd, members[id].pwd)) {         // Memo. pwd == members[id].pwd 는 적용이 안됨
+                    login = true;
+                    System.out.println("로그인 되었습니다.");
+                    break;
+                }
+            }
+        }
+        if (!login)
+            System.out.println("저희 은행 회원이 아닙니다. 회원가입을 해주세요.");
 
+        return login;
 
     }
 
-
     // 기존 가입 회원
-    public void memberManager(){
-        members[3] = new Member (4444, "Wendy", "abcd1234", 4320000.0);
-        members[4] = new Member (2222, "John", "qwer1234", 41150000.0);
-        members[5] = new Member (3333, "Suzy", "qwer4321", 60000000.0);
+    public void memberManager() {
+
+        members[0] = new Member(0, 1111, "Grace", "abcd1234", 5320000.0);
+        members[1] = new Member(1, 5555, "Julia", "abcd5678", 8320000.0);
+        members[2] = new Member(2, 6666, "Tom", "abcd8765", 49920000.0);
+        members[3] = new Member(3, 4444, "Wendy", "abcd1234", 4320000.0);
+        members[4] = new Member(4, 2222, "John", "qwer1234", 41150000.0);
+        members[5] = new Member(5, 3333, "Suzy", "qwer4321", 60000000.0);
+    }
+
+    public void deposit() {
+//        Deposit deposit = new Deposit();
+//        deposit.howMuch();
+        balance = members[id].balance;
+        System.out.print("입금할 금액을 입력해주세요: ");
+        Scanner sc = new Scanner(System.in);
+        double deposit = sc.nextDouble();
+
+        if (deposit > 0) {
+            balance += deposit;
+            members[id].balance = balance;
+            System.out.println("입금 후 잔액은 " + balance + "원 입니다.");
+            System.out.println("서비스를 이용해주셔서 감사합니다.");
+        } else {
+            System.out.println("입금하실 금액을 다시확인해 주세요.");
+            deposit();
+        }
+    }
+
+
+    public void withdraw() {
+        balance = members[id].balance;
+        System.out.print("출금할 금액을 입력해주세요: ");
+        Scanner sc = new Scanner(System.in);
+        double withdraw = sc.nextDouble();
+
+        if (balance >=  withdraw) {
+            balance -= withdraw;
+            members[id].balance = balance;
+            System.out.println("출금 후 잔액은 " + balance + "원 입니다.");
+            System.out.println("서비스를 이용해주셔서 감사합니다.");
+        } else {
+            System.out.println("출금하실 금액을 다시 확인해주세요.");
+            withdraw();
+        }
     }
 }
 
