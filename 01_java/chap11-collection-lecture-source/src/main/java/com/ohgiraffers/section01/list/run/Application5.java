aa7dfdbb788d0517a571778b622c726d7f089444
Leegiyeon <rldus3512@naver.com>
2024-01-23 11:37:36 +0900

    Java
    Stack, Queue 학습내용 Upload

---
+++ 01_java/chap11-collection-lecture-source/src/main/java/com/ohgiraffers/section01/list/run/Application5.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.section01.list.run;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Application5 {
+    public static void main(String[] args) {
+        /* 수업목표. Queue에 대해 이해하고 사용할 수 있다. */
+        /* 필기.
+         *  Queue란?
+         *  선형 메모리 공간에 데이터를 저장하여 순서유지를 위한 선입 선출 방식의 자료구조이다.
+         *  대부분 LinkedList를 많이 사용한다.
+        * */
+
+//        Queue<String> que = new LinkedList<>();
+        PriorityQueue<String> que = new PriorityQueue<>();          // Memo. Queue에서 정렬을 위한 최우선 가치에 있는 요소를 맨 앞으로 가져오는 기능을 지닌 자료구조
+        que.offer("first");
+        que.offer("second");
+        que.offer("third");
+        que.offer("fourth");
+        que.offer("fifth");
+
+        System.out.println("que = " + que);
+        System.out.println("peek(): " + que.peek());
+        System.out.println("peek(): " + que.peek());
+
+        System.out.println("poll(): " + que.poll());
+        System.out.println("poll(): " + que.poll());
+        System.out.println("poll(): " + que.poll());
+        System.out.println("poll(): " + que.poll());
+        System.out.println("poll(): " + que.poll());
+        System.out.println("que = " + que);
+
+    }
+}
