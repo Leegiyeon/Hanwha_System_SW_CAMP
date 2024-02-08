878d22cd7cbb701794dc7f3bd28550399d1a91db
Leegiyeon <rldus3512@naver.com>
2024-02-08 12:01:10 +0900

    JSP: Wrapper를 통한 Password 암호화 예제 Upload

--- 03_servlet/chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
+++ 03_servlet/chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
@@ -14,10 +14,10 @@ public class RequestWrapper extends HttpServletRequestWrapper {
 
         /* 설명. 'password'라는 키 값으로 getParameter 사용 시에 그 반환 값은 암호화해서 반환 */
         if("password".equals(key)) {
-            System.out.println("암호화 되기 전 value = " + super.getParameter("password"));
+            System.out.println("암호화 되기 전(평문:plain text) value = " + super.getParameter("password"));
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             value = passwordEncoder.encode(super.getParameter("password"));
-            System.out.println("암호화 된 value = " + value);
+            System.out.println("암호화 된(다이제스트) value = " + value);
         } else {
             /* 설명. 그 외의 키 값은 기존대로 동작 */
             value = super.getParameter(key);
