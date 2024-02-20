95576107bf497bb68ec1bdd617f51c079d8bf334
Leegiyeon <rldus3512@naver.com>
2024-02-20 10:44:12 +0900

    Mybatis: Select 활용 예제

---
+++ chap02-crud-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -0,0 +1,56 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        MenuController menuController = new MenuController();
+
+        do{
+            System.out.println("====== 메뉴관리 ======");
+            System.out.println("1. 메뉴 전체 조회");
+            System.out.println("2. 메뉴 코드로 메뉴 조회");
+            System.out.println("3. 신규 메뉴 추가");
+            System.out.println("4. 메뉴 수정");
+            System.out.println("5. 메뉴 삭제");
+            System.out.print("메뉴 관리 번호를 선택하세요: ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1:
+                    menuController.findAllMenus();
+                    break;
+                case 2:
+                    menuController.findMenuByMenuCode(inputMenuCode());
+                    break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+                case 9:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못된 메뉴를 입력하셨습니다.");
+            }
+
+
+        }while (true);
+
+    }
+
+    /* 설명. 사용자의 입력값을 Map형태로 반환 (Web에서는 key와 value로 request 객체에 담기는 parameter로 생각 )*/
+    private static Map<String, String> inputMenuCode() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("메뉴코드를 입력해주세요: ");
+        String menuCode = sc.nextLine();
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("menuCode", menuCode);
+
+        return parameter;
+    }
+}
---
+++ chap02-crud-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/MenuController.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import java.util.List;
+import java.util.Map;
+
+public class MenuController {
+
+
+    /* Memo. 컨트롤러 생성시 서비스는 반드시 따라와야 함 */
+    private final MenuService menuService;
+    private final PrintResult printResult;      // 결과 페이지에 해당하는 클래스
+
+    public MenuController() {
+        menuService = new MenuService();
+        printResult = new PrintResult();
+    }
+
+    public void findAllMenus() {
+        List<MenuDTO> menuList = menuService.findAllMenus();
+
+        if (!menuList.isEmpty()) {
+            printResult.printMenus(menuList);
+        }else printResult.printErrorMessage("조회할 메뉴가 없습니다.");
+    }
+
+    public void findMenuByMenuCode(Map<String, String> parameter) {
+
+        int menuCode = Integer.valueOf(parameter.get("menuCode"));
+
+        MenuDTO menu = menuService.findMenuByMenuCode(menuCode);
+
+        if (menu != null) {
+            printResult.printMenu(menu);
+        }else printResult.printErrorMessage(menuCode + "번의 메뉴는 없습니다.");
+    }
+}
