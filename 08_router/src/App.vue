2a382c71216951580efb06dcfaa9ee3da95a2108
Leegiyeon <rldus3512@naver.com>
2024-03-26 12:36:10 +0900

    Vue: Router에서 View 이동 및 값 불러오기

--- 08_router/src/App.vue
+++ 08_router/src/App.vue
@@ -2,7 +2,20 @@
   import {RouterLink, RouterView} from 'vue-router';
 </script>
 
-<!-- npm install vue-router@next -->
+<!-- npm install vue-router@next
+
+  RouterLink
+  사용자가 다른 라우터에 접속할 수 있도록하는 링크를 생성해준다.
+  RouterLink의 속성
+  1. to: 이동할 라우터 경로(uri)
+  2. acive-class: 현재 라우터와 일치할 때 적용할 css 클래스 지정
+
+  RouterView
+  현재 라우터에 해당하는 컴포넌트를 랜더링하는 지점을 정하기위해 사용된다.
+  vue-router가 현재 uri에 따라 매칭되는 컴포넌트를 찾으면, 해당 컴포넌트를 RouterView내부에 랜더링하여 이를통해 페이지 간의 전환을 구현할 수 있다.
+
+  일반적으로 RouterLink와 RouterView를 함께 사용한다.
+-->
 
 <template>
   <header>
@@ -11,8 +24,9 @@
     <div class="wrapper">
       <h2>basic router</h2>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/pathVariable">pathVariable</RouterLink>
+        <RouterLink to="/" active-class="active" replace>Home</RouterLink>
+        <RouterLink to="/pathvariable/1" active-class="active" replace>PathVariable</RouterLink>
+        <RouterLink to="/querystring?name=홍길동&age=20" active-class="active">QueryString</RouterLink>
       </nav>
     </div>
   </header>
---
+++ 08_router/src/views/01_router/QueryString.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>QueryString</h1>
+        <h2>currentRoute = {{ currentRoute.query }}</h2>
+        <h2>Name: {{ currentRoute.query.name }}</h2>
+        <h2>Age: {{ currentRoute.query.age }}</h2>
+    </div>
+</template>
+
+<script setup>
+    import {useRoute} from "vue-router";
+
+    const currentRoute = useRoute();
+</script>
+
+<style scoped>
+
+</style>
