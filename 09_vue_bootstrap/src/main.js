f00a2a8782dfc643ca1fe3ffe5c687f85e1093bd
Leegiyeon <rldus3512@naver.com>
2024-03-26 16:45:45 +0900

    Vue: 예시 자료 업로드

--- 09_vue_bootstrap/src/main.js
+++ 09_vue_bootstrap/src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 // bootstrap과 bootstrap-vue-3 insert 구문: 
 // npm install vue bootstrap bootstrap-vue-3
 import BootstrapVue3 from 'bootstrap-vue-3';
+// import { BCalendar } from 'bootstrap-vue'
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
@@ -12,4 +14,5 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 // createApp(App).mount('#app')
 const app = createApp(App)
 app.use(BootstrapVue3)
+// app.component('b-calendar', BCalendar)
 app.mount('#app')
